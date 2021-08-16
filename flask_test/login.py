a2f64e53783dc8941367c8cc72d30d92c634813b
张三 <zhangsan@yahoo.com>
2021-08-17 03:12:15 +0800

    模拟登陆

--- flask_test/login.py
+++ flask_test/login.py
@@ -10,4 +10,12 @@ def login():
     # dev 增加变量
     num4 = 4444
 
-    return "success"
+    username = "admin"
+    password = "******"
+    
+    if username == "admin" and password == "******":
+        return "登录成功!"
+    else:
+        return "登录失败!"
+    code = 200
+    return code
